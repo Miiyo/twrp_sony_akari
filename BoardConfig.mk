a5fd7d93cb2ada0c605c5a364096d222a1cca4b5
ekkusa <dayesofficial@gmail.com>
2023-04-06 18:09:19 +0800

    akari: Add Broken Rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,10 @@ DEVICE_PATH := device/sony/akari
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+# Broken Rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
